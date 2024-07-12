954920af3fbd2e5e67fa05a710324d05bf554298
rdmasquesalud <masquesaludrd@gmail.com>
2024-07-12 06:55:18 -0400

    Initial commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  pwa: {
+    name: 'My PWA App',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    // configure the workbox plugin
+    workboxPluginMode: 'GenerateSW',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      // swSrc: 'dev/sw.js',
+      // ...other Workbox options...
+    }
+  },
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000, // Puedes cambiar el puerto si lo deseas
+    disableHostCheck: true, // Esto deshabilita la verificación del host, útil para redes locales
+  }
+}
